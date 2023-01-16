4c6ffb870c45d3e84ce8e4a38689c2315a81fe38
Austin Hernandez <austin.jrh@gmail.com>
2023-01-16 15:56:02 +0800

    suspense

--- 20230112/sutd-frontend-hands-on/react-query/src/index.js
+++ 20230112/sutd-frontend-hands-on/react-query/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { QueryClient, QueryClientProvider } from "react-query"
-import { ReactQueryDevtools } from "react-query/devtools" // just for us to monitor the state of react-query
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools"; // just for us to monitor the state of react-query
 
-const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
